ef2e2adad9e0491af6e4644a5fbfde32f49360ba
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-05-08 11:47:47 +0200

    Set up initial product fetching component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoadMoreData from "./components/LoadMoreData";
+
+function App() {
+  return (
+    <div className="App">
+      <LoadMoreData />
+    </div>
+  );
+}
+
+export default App;
