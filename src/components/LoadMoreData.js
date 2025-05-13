596accfecd2d3b9d2fa0c2f18b75c0f0cf6138a1
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-05-13 10:56:26 +0200

    Add loading state and loading message to product fetch

--- src/components/LoadMoreData.js
+++ src/components/LoadMoreData.js
@@ -8,14 +8,22 @@ export default function LoadMoreData() {
 
   async function fetchProducts() {
     try {
+      setLoading(true);
       const response = await fetch(
         `https://dummyjson.com/products?limit=20&skip=${
           count === 0 ? 0 : count * 20
         }`
       );
       const result = await response.json();
+
+      if (result && result.products && result.products.length) {
+        setProducts(result.products);
+        setLoading(false);
+      }
+
       console.log(result);
     } catch (e) {
+      setLoading(false);
       console.log(e);
     }
   }
@@ -23,5 +31,10 @@ export default function LoadMoreData() {
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  if (loading) {
+    return <div>Loading data! Please wait...</div>;
+  }
+
   return <div className="container"></div>;
 }
